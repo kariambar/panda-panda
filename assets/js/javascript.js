109333ecfaed6c37fcd730569f6319ae7ae51f08
karina palacios <karina.spalacios@gmail.com>
2017-05-21 23:23:45 -0400

    funciones js

---
+++ assets/js/javascript.js
@@ -0,0 +1,10 @@
+//añadinendo js
+
+
+function fotos(){
+  var fotos = document.getElementsById('floresFotos').value = "";
+
+    var xBorrar = document.createElement('a');
+    xBorrar.setAttribute('class', 'eventoX');
+
+var link = document.getElementsByTagName('a').addEventListener('click', function(){
